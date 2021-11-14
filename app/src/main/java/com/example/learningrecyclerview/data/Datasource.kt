d6525fe53e4d5071cf449d2797f859b325e30169
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-11-14 01:40:45 +0100

    Learning RecyclerView

---
+++ app/src/main/java/com/example/learningrecyclerview/data/Datasource.kt
@@ -0,0 +1,20 @@
+package com.example.learningrecyclerview.data
+
+import com.example.learningrecyclerview.R
+import com.example.learningrecyclerview.model.Affirmation
+
+class Datasource {
+    fun loadAffirmation(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10))
+    }
+}
---
+++ app/src/main/java/com/example/learningrecyclerview/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.example.learningrecyclerview.model
+
+data class Affirmation(val stringResourceId: Int)
