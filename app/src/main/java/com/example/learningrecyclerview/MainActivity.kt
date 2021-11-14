3ab01623e6cb978232fc067f6b47e167a6e9a130
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-11-14 10:39:30 +0100

    Text Affirmation completed

--- app/src/main/java/com/example/learningrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/learningrecyclerview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.learningrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.learningrecyclerview.adapter.ItemAdapter
+import com.example.learningrecyclerview.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmation()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
